072eed4986d159ccce33a56c6171568d10d1ebed
tharindu09705 <ytruwan252@gmail.com>
2024-12-21 06:21:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+llconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=bcNChKDB#FiaEr-16HdfmhL9AlQaK6KyLe0KrKOHT8LcDdLj3O3Y' : process.env.SESSION_ID,
+SESSION_ID: process.env.S0pESSION_ID === undefined ? 'VAJIRA-MD=bcNChKDB#FiaEr-16HdfmhL9AlQaK6KyLe0KrKOHT8LcDdLj3O3Y' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
